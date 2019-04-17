bdccc1ea8530c08693d95ee15b8b887bf01d5b0c
pra5ha <47806864+pra5ha@users.noreply.github.com>
2019-04-17 13:06:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_launch_configuration" "nv-dev-watermark-launch-config-07Feb2019" {
 resource "aws_autoscaling_group" "nv-dev-watermark-asg" {
         name = "nv-dev-watermark-asg"
         availability_zones = ["${var.avaliblity_zone}"]
-        launch_configuration = "${aws_launch_configuration.my1-launch-configuration.id}"
+        launch_configuration = "${aws_launch_configuration.nv-dev-watermark-launch-config-07Feb2019.id}"
         vpc_zone_identifier = ["${var.subnet_id}"]
         desired_capacity   = "${var.desired_count}"
         min_size = "${var.min_size}"
